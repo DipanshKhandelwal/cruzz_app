47c3e3090f838dfd5627ad13c60583c605a47c6f
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-10-24 23:59:38 +0530

    Add explore tab

---
+++ src/UI/components/Main/Tabs/Explore/Explore.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import { View, Text } from "react-native";
+
+class Explore extends Component {
+
+    render() {
+        return(
+            <View>
+                <Text>
+                    Explore
+                </Text>
+            </View>
+            );
+    }
+}
+
+export default Explore;
