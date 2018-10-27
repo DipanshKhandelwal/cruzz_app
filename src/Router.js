1f2015e2311ab14f377845ba93414a6b62be1bca
DipanshKhandelwal <deepanshrocks438@gmail.com>
2018-10-27 11:29:56 +0530

    Update router change the drawer UI and new options

--- src/Router.js
+++ src/Router.js
@@ -1,74 +1,122 @@
 import React from 'react';
-import { createStackNavigator, createMaterialTopTabNavigator } from 'react-navigation';
+import { createStackNavigator, createMaterialTopTabNavigator, createDrawerNavigator } from 'react-navigation';
 import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs'
 import Icon from 'react-native-vector-icons/FontAwesome';
-
+import Image from 'react-native-remote-svg';
+import {Thumbnail, View} from 'native-base';
 
 import LoginForm from './UI/components/Authentication/LoginForm/LoginForm';
 import SignupForm from './UI/components/Authentication/SignupForm/SignupForm';
+import SplashScreen from './UI/components/SplashScreen/SplashScreen';
+import CustomDrawerContentComponent from './UI/components/Main/Drawer/CustomDrawerContentComponent/CustomDrawerContentComponent';
 import Feed from './UI/components/Main/Tabs/Feed/Feed'
 import Profile from './UI/components/Main/Tabs/Profile/Profile'
 import Explore from './UI/components/Main/Tabs/Explore/Explore'
 
 const RootStack = createStackNavigator(
   {
+    SplashScreen: { screen: SplashScreen},
     Login: { screen: LoginForm},
     SignUp: { screen: SignupForm},
-    Main: {
-      screen:  createMaterialTopTabNavigator(
+    Drawer: {
+      screen:  createDrawerNavigator(
       {
-      Feed:
-      { screen: Feed,
-        navigationOptions: {
-          tabBarLabel:"Feeds Page",
-          tabBarIcon: ({ tintColor }) => (
-            <Icon name="newspaper-o" size={20} color="#131313" />
-          )
+        Main: {
+          screen:  createMaterialTopTabNavigator(
+          {
+          Feed:
+          { screen: Feed,
+            navigationOptions: {
+              tabBarLabel:"Feeds Page",
+              tabBarIcon: ({ tintColor }) => (
+                <Icon name="feed" size={20} color="#39acef" />
+              )
+            },
+          },
+          Profile:
+          {screen: Profile,
+            navigationOptions: {
+              tabBarLabel:"Profile Page",
+              tabBarIcon: ({ tintColor }) => (
+                <Icon name="user" size={20} color="#ff3838" />
+              ),
+            }
+          },
+          Explore:
+          {screen: Explore,
+            navigationOptions: {
+              tabBarLabel:"Explore Page",
+              tabBarIcon: ({ tintColor }) => (
+                <Icon name="search" size={20} color="#3e37ff" />
+              ),
+            }
+          }
+         },
+         {
+          tabBarPosition: 'bottom',
+          initialRouteName: 'Feed',
+          animationEnabled: true,
+          tabBarOptions: { 
+            showIcon: true,
+            showLabel: false,
+            pressColor: '#39acef',
+            style:{
+              backgroundColor: '#ffffff',
+              borderTopWidth:1,
+              borderTopColor:'#D3D3D3'
+            },
+            indicatorStyle: {
+              backgroundColor: "#39acef",
+            }
+          },
+         }),
+          navigationOptions: {
+            drawerLabel: 'Home',
+            drawerIcon: ({ tintColor }) => (
+              <View>
+                <Icon name="home" size={25} color="#39acef" />
+              </View>
+            ),
+          }
+        },
+        Feed:
+        { screen: Feed,
+          navigationOptions: {
+            tabBarLabel:"Feeds Page",
+            tabBarIcon: ({ tintColor }) => (
+              <Icon name="feed" size={20} color="#39acef" />
+            )
+          },
+        },
+        Profile:
+        {screen: Profile,
+          navigationOptions: {
+            tabBarLabel:"Profile Page",
+            tabBarIcon: ({ tintColor }) => (
+              <Icon name="user" size={20} color="#ff3838" />
+            ),
+          }
+        },
+        Explore:
+        {screen: Explore,
+          navigationOptions: {
+            tabBarLabel:"Explore Page",
+            tabBarIcon: ({ tintColor }) => (
+              <Icon name="search" size={20} color="#3e37ff" />
+            ),
+          }
         }
-      },
-      Profile:
-      {screen: Profile,
-        navigationOptions: {
-          tabBarLabel:"Profile Page",
-          tabBarIcon: ({ tintColor }) => (
-            <Icon name="user-o" size={20} color="#131313" />
-          ),
-        }
-      },
-      Explore:
-      {screen: Explore,
-        navigationOptions: {
-          tabBarLabel:"Explore Page",
-          tabBarIcon: ({ tintColor }) => (
-            <Icon name="handshake-o" size={20} color="#131313" />
-          ),
-        }
-       }
      },
      {
-      tabBarPosition: 'bottom',
-      initialRouteName: 'Feed',
-      animationEnabled: true,
-      tabBarOptions: { 
-        showIcon: true,
-        showLabel: false,
-        activeTintColor: '#ffb642',
-        inactiveTintColor: '#c42929',
-        style:{
-          backgroundColor: '#5a90e8',
-          borderTopWidth:1,
-          borderTopColor:'#D3D3D3'
-        },
-        indicatorStyle: {
-          backgroundColor: "#081428",
-        }
-      },
+      initialRouteName: 'Main',
+      backBehavior: "initialRoute",
+      contentComponent: CustomDrawerContentComponent,
      }),
-    }
+    },
   },
   {
-    initialRouteName: 'Main',
-    navigationOptions : {
+    initialRouteName: 'Drawer',
+    navigationOptions: {
       header: null
     }
   },
