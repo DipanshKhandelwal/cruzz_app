9cf569ae416b94ef01a5f094bd8729514d868a04
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-10-31 15:48:10 +0530

    Update profile tab

--- src/UI/components/Main/Tabs/Profile/Profile.js
+++ src/UI/components/Main/Tabs/Profile/Profile.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import { ScrollView, View, Text } from "react-native";
 import CardOne from "../../Cards/CardOne";
 import CardTwo from "../../Cards/CardTwo";
-import { Left, Right, Body, ListItem, Thumbnail } from "native-base";
+import { Left, Right, Body, ListItem, Thumbnail, Input, Item, Content, Textarea } from "native-base";
 import MyHeader from '../../MyHeader/MyHeader';
 
 
@@ -12,23 +12,67 @@ class Profile extends Component {
         return(
             <ScrollView>
               <MyHeader name="Profile" click={()=>this.props.navigation.toggleDrawer()} color='#ff3838' />
+
+
               <View style={{
                   backgroundColor: 'white',
-
+                  display: 'flex',
+                  flexDirection: 'column',
+                  borderRadius: 5,
+                  shadowColor: '#ffffff',
+                  shadowOpacity: 1,
+                  shadowOffset: { width: 5, height: 5 },
+                  margin: 5,
+                  elevation: 3
               }}>
-                <ListItem avatar>
-                  <Left>
-                    <Thumbnail source={require('../../../../res/load.gif')} />
-                  </Left>
-                  <Body>
-                    <Text>Monal Shadi</Text>
-                    <Text note>Hamesha Lagta H Apun Hi Bhagwaan H . .</Text>
-                  </Body>
-                  <Right>
-                    <Text note>3:43 pm</Text>
-                  </Right>
-                </ListItem>
+                <View style={{
+                  backgroundColor: '#3f3f3f',
+                  display: 'flex',
+                  flex :1,
+                  height: 100
+                }} />
+
+                <View style={{
+                  backgroundColor: '#ffffff',
+                  display: 'flex',
+                  flex :1,
+                  height: 90
+                }} />
+
+                <View                
+                  style={{
+                    position: 'absolute',
+                    alignSelf: 'center',
+                    top: 60,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    justifyContent: 'center'
+                  }} >
+                  <Thumbnail
+                    source={require('../../../../../images/profile.jpg')}
+                  />
+                  <Text style={{fontSize: 25, fontWeight: 'bold'}} >Monal Shadi</Text>
+                  <Text>Daddu ka status</Text>
+                </View>
               </View>
+              <View style={{
+                  backgroundColor: 'white',
+                  display: 'flex',
+                  flexDirection: 'column',
+                  marginLeft: 5,
+                  marginRight: 5,
+                  borderRadius: 5,
+                  shadowColor: '#ffffff',
+                  shadowOpacity: 1,
+                  shadowOffset: { width: 5, height: 5 },
+                  elevation: 3
+              }}>
+
+                <Textarea rowSpan={5} placeholder="Whats in your mind !!" style={{ opacity: 0.6 }} />
+
+              </View>
+
               <CardTwo/>
               <CardOne/>
               <CardTwo/>
