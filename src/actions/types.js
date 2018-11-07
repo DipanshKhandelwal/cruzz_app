81f26a61dd90d32c88cae570334f0b45f4f6c3f0
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-11-08 03:02:32 +0530

    Removed typo in action types

--- src/actions/types.js
+++ src/actions/types.js
@@ -5,7 +5,7 @@ export const LOGIN_USER_FAIL = 'login_user_fail';
 export const LOGIN_USER = 'login_user';
 
 export const SIGN_UP_EMAIL_CHANGED = 'sign_up_email_changed';
-export const SIGN_UP_USERNAME_CHANGED = 'sign_up_email_changed';
+export const SIGN_UP_USERNAME_CHANGED = 'sign_up_username_changed';
 export const SIGN_UP_PASSWORD_CHANGED = 'sign_up_password_changed';
 export const SIGN_UP_CONFIRM_PASSWORD_CHANGED = 'sign_up_confirm_password_changed';
 export const SIGN_UP_USER_SUCCESS = 'sign_up_user_success';
