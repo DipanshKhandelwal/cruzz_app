bde06250107cdbb4ce6b821641b64d85111af774
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-11-09 20:02:21 +0530

    Added Data fetch and update in feed

--- src/UI/components/Main/Tabs/Feed/Feed.js
+++ src/UI/components/Main/Tabs/Feed/Feed.js
@@ -1,14 +1,40 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
-import { View, ScrollView } from "react-native";
+import { View, ScrollView, RefreshControl } from "react-native";
 import CardOne from '../../Cards/CardOne'
-import CardTwo from '../../Cards/CardTwo'
 import MyHeader from '../../MyHeader/MyHeader'
 import { ListItem, Thumbnail, Text , Left, Body, Right } from 'native-base';
 import Status from '../../Status/Status.js';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import {
+    fetchFeed,
+    postReset,
+    postDescriptionChanged,
+    postBodyChanged,
+    postTitleChanged,
+    userSelected,
+    postSelected
+} from '../../../../../actions';
 
 class Feed extends Component {
 
+    componentDidMount() {
+        this.props.fetchFeed(this.props.user);
+        this.props.postReset();
+    }
+
+    _onRefresh = () => {
+        this.props.fetchFeed(this.props.user);
+        this.props.postReset();
+    }
+    
     render() {
+
+        if(!this.props.user){
+            this.props.navigation.navigate('Login')
+        }
+
         return(
             <View style={{
                 width: '100%',
@@ -21,29 +47,68 @@ class Feed extends Component {
                     <MyHeader name="Feed" click={()=>this.props.navigation.toggleDrawer()} color='#39acef'/>
                     <ListItem avatar>
                         <Left>
-                            <Thumbnail source={require('../../../../res/load.gif')} />
+                        {
+                            this.props.profile ? 
+                            <Thumbnail
+                                source={{ uri: this.props.profile.image }}
+                            />
+                            : 
+                            <Thumbnail source={require('../../../../../images/profile.jpg')} style={{ marginBottom: 10 }} />
+                        }
                         </Left>
                         <Body>
-                            <Text>Monal Shadi</Text>
-                            <Text note>Hamesha Lagta H Apun Hi Bhagwaan H . .</Text>
+                            <Text>
+                                {
+                                    this.props.user ? this.props.user.username : "User Name"
+                                }
+                            </Text>
+                            <Text note> Status . . . </Text>
                         </Body>
                         <Right>
                             <Text note>3:43 pm</Text>
                         </Right>
                     </ListItem>
                 </View>
-                <ScrollView>
-                    <Status />
-                    <CardTwo/>
-                    <CardOne/>
-                    <CardTwo/>
-                    <CardOne/>
-                    <CardTwo/>
-                    <CardOne/>
+                <ScrollView
+                    refreshControl={
+                        <RefreshControl
+                            refreshing={this.props.loading}
+                            onRefresh={this._onRefresh}
+                        />
+                    }
+                >
+                    <Status press={()=> this.props.navigation.navigate('Post')}/>
+                    {
+                        _.map(this.props.feed, (item, index)=>
+                            <CardOne
+                                key={index}
+                                post={item}
+                            />
+                        )
+                    }
                 </ScrollView>
             </View>
             );
     }
 }
 
-export default Feed;
+const mapStatetoProps = (state) => {
+    const { user, profile } = state.auth;
+    const { feed, loading } = state.feed;
+    const { post } = state.post;
+    return{ user, profile, feed, loading };
+};
+
+const matchDispatchToProps = ( dispatch ) => {
+    return bindActionCreators({
+        fetchFeed: fetchFeed,
+        postReset: postReset,
+        postDescriptionChanged: postDescriptionChanged,
+        postBodyChanged: postBodyChanged,
+        postTitleChanged: postTitleChanged,
+        userSelected: userSelected,
+        postSelected: postSelected
+    }, dispatch);
+}
+
+export default connect(mapStatetoProps, matchDispatchToProps )(Feed);
