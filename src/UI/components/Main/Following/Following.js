8d386ee0ac919a11d1dcd77946a712bafbe1001f
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-11-09 19:59:32 +0530

    Added Following component for showing followers and following

---
+++ src/UI/components/Main/Following/Following.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Textarea, Button } from 'native-base';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+const following = (props) => {
+    return(
+        <View style={{
+            backgroundColor: 'white',
+            display: 'flex',
+            flexDirection: 'column',
+            marginLeft: 5,
+            marginRight: 5,
+            marginTop:5,
+            borderRadius: 5,
+            shadowColor: '#ffffff',
+            shadowOpacity: 1,
+            shadowOffset: { width: 5, height: 5 },
+            elevation: 3
+        }}>
+
+            <View
+                style={{ 
+                    display: 'flex',
+                    flexDirection: 'row',
+                 }} >
+
+                <Button
+                    transparent
+                    style={{
+                        flex :1,
+                        justifyContent: 'center',
+                        flexDirection: 'column',
+                        padding: 5,
+                        backgroundColor: '#dededeb5',
+                        borderRadius: 0,
+                        borderBottomLeftRadius: 5,
+                        borderRightColor: '#ffffff15',
+                        borderRightWidth: .5
+                    }}>
+                    <Text style={{fontWeight: 'bold', color:'#1979e8c9'}} >
+                        { props.followers }
+                    </Text>
+                    <Text style={{fontWeight: 'bold'}} >Followers</Text>
+                </Button>
+
+                <Button
+                    transparent
+                    style={{
+                        flex :1,
+                        justifyContent: 'center',
+                        flexDirection: 'column',
+                        padding: 5,
+                        backgroundColor: '#dededeb5',
+                        borderRadius: 0,
+                        borderBottomRightRadius: 5,
+                    }}>
+                    <Text style={{fontWeight: 'bold', color:'#1979e8c9'}} >
+                        { props.following }
+                    </Text>
+                    <Text style={{fontWeight: 'bold'}} >Following</Text>
+                </Button>
+
+            </View>
+        </View>
+    );
+}
+
+export default following;
