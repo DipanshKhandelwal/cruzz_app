26a405b9405d906714fa244463aed7dfac7dd420
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2018-11-15 23:48:03 +0530

    Update help page

--- src/UI/components/Help/Help.js
+++ src/UI/components/Help/Help.js
@@ -1,13 +1,22 @@
 import React, {Component} from 'react';
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 
 class Help extends Component {
     render() {
         return(
-            <View>
-                <Text>
-                    Help
+            <View style={{
+                display: 'flex',
+                flexDirection: 'column',
+                flex: 1,
+                alignItems: 'center',
+                justifyContent: 'center'
+            }} >
+                <Text style={{ fontSize: 20 }} >
+                    Contact Us : 
                 </Text>
+                <TouchableOpacity onPress={()=>Linking.openURL('mailto:201652013@iiitvadodara.ac.in')} >
+                        <Text style={{fontSize: 20, color: 'blue'}} >help@vconnect.com</Text>
+                </TouchableOpacity>
             </View>
             );
     }
